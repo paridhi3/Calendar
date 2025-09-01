86ef5e82594dcda6154fa545c8d694d483fe01e0
Paridhi <pari9901@gmail.com>
2025-09-01 19:09:56 +0530

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { ReactNode } from "react";
 import Provider from "./provider";
-import Header from '../components/Header'
+import Header from "../components/Header";
 
 export const metadata = {
   title: "Calendar",
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/logo.png" type="image/png" />
+      </head>
       <body>
         <div>
           <Provider>
